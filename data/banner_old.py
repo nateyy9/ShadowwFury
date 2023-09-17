72c38517205283364ca5e35ba15d41a3219008d8
nateyy <145199674+nateyy9@users.noreply.github.com>
2023-09-17 19:00:27 +0200

    Add files via upload

---
+++ data/banner_old.py
@@ -0,0 +1,14 @@
+from colorama import Fore
+
+def banner():
+    _ver_ = '1.0.1'   
+    print(Fore.RED + '    =/\\                 /\\= ')
+    print(Fore.RED + '    / \\ ._   (\\_/)   _./ \\ ') 
+    print(Fore.RED + '   / .\'\'._\'--(o.o)--\'_.\'\' \\ ')
+    print(Fore.RED + '  /.\' _/ |`\'=/ " \\=\'`| \\_ `.\'')
+    print(Fore.RED + ' /` .\' `\\;,-\'\\___/\'-;/` \'. \\')
+    print(Fore.RED + '/.-\'       `\\(-V-)/`       `-.\'')
+    print(Fore.RED + '`            "   "            `  ')   
+    print(Fore.RED + f'\x1b[31m\n ShadowwFury by nateyy, version {_ver_}')
+
+banner()
